6522e1341605013b4b2abb3eca847d20def179a2
Dinesh-Khanal <dinesh_Khanal@hotmail.com>
2023-02-05 12:29:41 +0545

    in controller small typescript change made

--- backend/controller/teacherController.ts
+++ backend/controller/teacherController.ts
@@ -5,16 +5,18 @@ import AppError from "../utils/appError";
 import fs from "fs";
 import path from "path";
 
-export const getTeachers = asyncHandler(async (req: Request, res: Response) => {
-  const teachers = await Teacher.find();
-  if (teachers) {
-    res.status(200).json(teachers);
-  } else {
-    throw new AppError("Could not found teacher data", 500);
+export const getTeachers = asyncHandler(
+  async (req: Request, res: Response): Promise<void> => {
+    const teachers = await Teacher.find();
+    if (teachers) {
+      res.status(200).json(teachers);
+    } else {
+      throw new AppError("Could not found teacher data", 500);
+    }
   }
-});
+);
 export const createTeacher = asyncHandler(
-  async (req: Request, res: Response) => {
+  async (req: Request, res: Response): Promise<void> => {
     const photo = req.file?.filename;
     const { name, address, fatherName, email, subject, dob } = req.body;
     const teacher = await Teacher.create({
@@ -37,7 +39,7 @@ export const createTeacher = asyncHandler(
   }
 );
 export const updateTeacher = asyncHandler(
-  async (req: Request, res: Response) => {
+  async (req: Request, res: Response): Promise<void> => {
     const photo = req.file?.filename;
     const teacher = await Teacher.findByIdAndUpdate(req.params.id, {
       ...req.body,
@@ -51,7 +53,7 @@ export const updateTeacher = asyncHandler(
   }
 );
 export const deleteTeacher = asyncHandler(
-  async (req: Request, res: Response) => {
+  async (req: Request, res: Response): Promise<void> => {
     const teacher = await Teacher.findById(req.params.id);
     if (!teacher) {
       throw new AppError("Could not found teacher data", 500);
